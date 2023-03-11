620a222f6fa5aae7d518ba5a4a7e2788188ff2dc
Niklas Avermann <n.avermann@gmx.de>
2023-03-11 14:46:29 +0100

    stuff

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzbuzz(n) {
+    let result = '';
+  
+    if (n % 3 === 0 && n % 5 === 0) {
+      result = 'FizzBuzz';
+    } else if (n % 3 === 0) {
+      result = 'Fizz';
+    } else if (n % 5 === 0) {
+      result = 'Buzz';
+    }
+  
+    console.log(result);
+    return result;
+  }  
+
+fizzbuzz(35); // Call the function with input 15
---
+++ stuff.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+
+output = fizzbuzz(5) # Call the function with input 15
+print(output) # Output the result for input 15
